7cacd47d4e6d2a61936df7bac8a1fea627906804
itmecat2058 <jmorley2007@gmail.com>
2025-03-12 12:00:57 -0400

    added sounds and functions for more sounds

--- main.js
+++ main.js
@@ -3,6 +3,42 @@ function playQuack() {
     audioQuack.play()
 }
 
-function playClash() {
+function playCan() {
+    var audioCan = new Audio(`sounds/`)
+    audioCan.play()
+}
 
+function playClash() {
+    var audioClash = new Audio(`sounds/`)
+    audioClash.play()
+}
+
+function playPokemon() {
+    var audioPokemon = new Audio(`sounds/`)
+    audioPokemon.play()
+}
+
+function playMegalovania() {
+    var audioMegalovania = new Audio(`sounds/`)
+    audioMegalovania.play()
+}
+
+function playRoll() {
+    var audioRoll = new Audio(`sounds/`)
+    audioRoll.play()
+}
+
+function playLego() {
+    var audioLego = new Audio(`sounds/`)
+    audioLego.play()
+}
+
+function playOOF() {
+    var audioOOF = new Audio(`sounds/`)
+    audioOOF.play()
+}
+
+function playDooong() {
+    var audioDooong = new Audio(`sounds/dooong.mp3`)
+    audioDooong.play()
 }
