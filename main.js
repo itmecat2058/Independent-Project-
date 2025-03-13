55ca149721c77076678058042f1b114d6cef13d3
itmecat2058 <jmorley2007@gmail.com>
2025-03-13 12:02:19 -0400

    refactor JS, add custom sound uploader

--- main.js
+++ main.js
@@ -1,44 +1,11 @@
-function playQuack() {
-    var audioQuack = new Audio(`sounds/quack`)
-    audioQuack.play()
-}
+function playSound(audio) {
+    var sound = new Audio(`sounds/${audio}.mp3`);
+    sound.play();
+};
 
-function playCan() {
-    var audioCan = new Audio(`sounds/`)
-    audioCan.play()
-}
-
-function playClash() {
-    var audioClash = new Audio(`sounds/`)
-    audioClash.play()
-}
-
-function playPokemon() {
-    var audioPokemon = new Audio(`sounds/`)
-    audioPokemon.play()
-}
-
-function playMegalovania() {
-    var audioMegalovania = new Audio(`sounds/`)
-    audioMegalovania.play()
-}
-
-function playRoll() {
-    var audioRoll = new Audio(`sounds/`)
-    audioRoll.play()
-}
-
-function playLego() {
-    var audioLego = new Audio(`sounds/`)
-    audioLego.play()
-}
-
-function playOOF() {
-    var audioOOF = new Audio(`sounds/`)
-    audioOOF.play()
-}
-
-function playDooong() {
-    var audioDooong = new Audio(`sounds/dooong.mp3`)
-    audioDooong.play()
-}
+function playCustSound() {
+    var musicFile = document.getElementById("musicFile").files[0];
+    var musicPlayer = document.getElementById("musicPlayer");
+    musicPlayer.src = URL.createObjectURL(musicFile);
+    musicPlayer.play();
+  }
